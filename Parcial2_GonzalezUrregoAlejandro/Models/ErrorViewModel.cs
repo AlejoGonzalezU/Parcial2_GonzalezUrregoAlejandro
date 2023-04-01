13026dd96f8269997fe90ab4d317c5cae5a14c45
alejandro.gonzalez01 <alejandro.gonzalez01@globant.com>
2023-04-01 17:25:22 -0500

    Initial commit with automatically created files.

---
+++ Parcial2_GonzalezUrregoAlejandro/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Parcial2_GonzalezUrregoAlejandro.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
